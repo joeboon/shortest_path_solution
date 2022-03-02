36516c835160f652558fd731cde68064209209a6
Chelsea Troy <chelsea.dommert@gmail.com>
2022-03-02 12:28:21 -0600

    Columns test

--- shortest_path.py
+++ shortest_path.py
@@ -4,6 +4,23 @@ class Board():
     def __init__(self, rows):
         self.rows = rows
 
+def test_column():
+    board = Board([
+        ['S', 0, 0, 0],
+        [ 1,  1, 0, 0],
+        [ 0,  0, 0, 0],
+        ['E', 0, 0, 0],
+
+    ])
+
+    result = board.columns(1)
+
+    if result == [0, 1, 0, 0]:
+        print("SUCCESS!")
+    else:
+        raise Exception(f"Whoops, columns should have been [0, 1, 0, 0] but was {result}.")
+
+test_column()
 
 def shortest_path(board):
     return 0
